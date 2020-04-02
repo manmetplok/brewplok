66c01f1e4bc8e21bd1cf63b113d0f00e7659c36b
Jorrit Everts <jorrit@baseflow.com>
2020-04-03 00:13:17 +0200

    Remove build

--- build/CMakeFiles/brewplok_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/brewplok_autogen"
-  "brewplok_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/brewplok_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
